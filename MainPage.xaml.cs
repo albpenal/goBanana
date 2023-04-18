38a3afcca9dc5d54719c279b70c505b2d187a1e2
sosanc03 <99793450+sosanc03@users.noreply.github.com>
2023-04-18 20:33:08 +0200

    glosario, juego y aspectos

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI;
 using System.Diagnostics;
@@ -74,6 +75,10 @@ namespace goBanana
 
             options.Visibility = Visibility.Visible;
             info.Visibility = Visibility.Collapsed;
+            game.Visibility = Visibility.Collapsed;
+            skinsButtons.Visibility = Visibility.Collapsed;
+            skinsText.Visibility = Visibility.Collapsed;
+            simiopedia.Visibility = Visibility.Collapsed;
         }
 
         private void InfoButton_Click(object sender, RoutedEventArgs e)
@@ -86,6 +91,10 @@ namespace goBanana
 
             info.Visibility = Visibility.Visible;
             options.Visibility = Visibility.Collapsed;
+            game.Visibility = Visibility.Collapsed;
+            skinsButtons.Visibility = Visibility.Collapsed;
+            skinsText.Visibility = Visibility.Collapsed;
+            simiopedia.Visibility = Visibility.Collapsed;
         }
 
         private void GameButton_Click(object sender, RoutedEventArgs e)
@@ -96,8 +105,12 @@ namespace goBanana
             SKINS.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xDD, 0x5F));
             GLOSARIO.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xDD, 0x5F));
 
+            game.Visibility = Visibility.Visible;
             options.Visibility = Visibility.Collapsed;
             info.Visibility = Visibility.Collapsed;
+            skinsButtons.Visibility = Visibility.Collapsed;
+            skinsText.Visibility = Visibility.Collapsed;
+            simiopedia.Visibility = Visibility.Collapsed;
         }
 
         private void SkinsButton_Click(object sender, RoutedEventArgs e)
@@ -108,8 +121,12 @@ namespace goBanana
             OPCIONES.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xDD, 0x5F));
             GLOSARIO.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xDD, 0x5F));
 
+            skinsButtons.Visibility = Visibility.Visible;
+            skinsText.Visibility = Visibility.Visible;
             options.Visibility = Visibility.Collapsed;
             info.Visibility = Visibility.Collapsed;
+            game.Visibility = Visibility.Collapsed;
+            simiopedia.Visibility = Visibility.Collapsed;
         }
 
         private void GlosaryButton_Click(object sender, RoutedEventArgs e)
@@ -120,8 +137,12 @@ namespace goBanana
             SKINS.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xDD, 0x5F));
             OPCIONES.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xDD, 0x5F));
 
+            simiopedia.Visibility = Visibility.Visible;
             options.Visibility = Visibility.Collapsed;
             info.Visibility = Visibility.Collapsed;
+            game.Visibility = Visibility.Collapsed;
+            skinsButtons.Visibility = Visibility.Collapsed;
+            skinsText.Visibility = Visibility.Collapsed;
         }
 
         private void EnglishButton_Click(object sender, RoutedEventArgs e)
@@ -147,7 +168,6 @@ namespace goBanana
             FRENCH.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xDD, 0x5F));
             GERMAN.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xDD, 0x5F));
         }
-
 
         private void GermanButton_Click(object sender, RoutedEventArgs e)
         {
@@ -179,6 +199,8 @@ namespace goBanana
             JUEGO.Visibility = Visibility.Visible;
             SKINS.Visibility = Visibility.Visible;
             GLOSARIO.Visibility = Visibility.Visible;
+            INFO.Visibility = Visibility.Visible;
+            OPCIONES.Visibility = Visibility.Visible;
         }
 
         private void BuyCursor_Click(object sender, RoutedEventArgs e)
@@ -209,8 +231,82 @@ namespace goBanana
             {
                 NumberOfTractors.Text = (int.Parse(NumberOfTractors.Text) + 1).ToString();
                 BananaCount.Text = (int.Parse(BananaCount.Text) - int.Parse(TractorPrice.Text)).ToString();
-                TractorPrice.Text = (int.Parse(TractorPrice.Text) + 1).ToString();
+                TractorPrice.Text = (int.Parse(TractorPrice.Text) + 2000).ToString();
             }
+        }
+
+        private void Clown_Click(object sender, RoutedEventArgs e)
+        {
+            if (Clowns.Text == "COMPRAR" && 10 <= int.Parse(BananaCount.Text))
+            {
+                BananaCount.Text = (int.Parse(BananaCount.Text) - 10).ToString();
+                ClownPrice.Text = "Equipar";
+                Clowns.Text = "COMPRADO";
+            }
+            else if(Clowns.Text == "COMPRADO" && ClownPrice.Text == "Equipar")
+            {
+                Desequipar.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xDD, 0x5F));
+                CurrentSkin.Text = "PAYASO";
+                ClownPrice.Text = "Equipado";
+                if (VikingPrice.Text == "Equipado") VikingPrice.Text = "Equipar";
+            }
+        }
+
+        private void Viking_Click(object sender, RoutedEventArgs e)
+        {
+            Desequipar.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xDD, 0x5F));
+            if (Vikings.Text == "COMPRAR" && 20 <= int.Parse(BananaCount.Text))
+            {
+                BananaCount.Text = (int.Parse(BananaCount.Text) - 20).ToString();
+                VikingPrice.Text = "Equipar";
+                Vikings.Text = "COMPRADO";
+            }
+            else if (Vikings.Text == "COMPRADO" && VikingPrice.Text == "Equipar")
+            {
+                CurrentSkin.Text = "VIKINGO";
+                VikingPrice.Text = "Equipado";
+                if (ClownPrice.Text == "Equipado") ClownPrice.Text = "Equipar";
+            }
+        }
+
+        private void Desequipar_Click(object sender, RoutedEventArgs e) 
+        { 
+            Desequipar.Background = new SolidColorBrush(Color.FromArgb(0xFE, 0xFE, 0xCD, 0x13));
+
+            CurrentSkin.Text = "NINGUNO";
+
+            if(ClownPrice.Text == "Equipado") ClownPrice.Text = "Equipar";
+            else if (VikingPrice.Text == "Equipado") VikingPrice.Text = "Equipar";
+        }
+
+        private void next_Click(object sender, RoutedEventArgs e)
+        {
+            monete.Visibility = Visibility.Collapsed;
+            orangutan.Visibility = Visibility.Visible;
+
+            //string s = System.IO.Directory.GetCurrentDirectory() + "\\" + "Assets\\noflecha.jpg";
+            //nextBImage.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(s));
+
+            //s = System.IO.Directory.GetCurrentDirectory() + "\\" + "Assets\\flecha.jpg";
+            //backBImage.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(s));
+
+            //nextBImage.Source = new BitmapImage(new Uri("Assets/noflecha.jpg", UriKind.Relative));
+            //backBImage.Source = new BitmapImage(new Uri("Assets/flecha.jpg", UriKind.Relative));
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            monete.Visibility = Visibility.Visible;
+            orangutan.Visibility = Visibility.Collapsed;
+
+            //string s = System.IO.Directory.GetCurrentDirectory() + "\\" + "Assets\\flecha.jpg";
+            //nextBImage.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(s));
+
+            //nextBImage.Source = new BitmapImage(new Uri("Assets/flecha.jpg", UriKind.Relative));
+            //backBImage.Source = new BitmapImage(new Uri("Assets/noflecha.jpg", UriKind.Relative));
+
+            //s = System.IO.Directory.GetCurrentDirectory() + "\\" + "Assets\\noflecha.jpg";
+            //backBImage.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(s));
         }
         private async void GoGithub_Click(object sender, RoutedEventArgs e)
         {
